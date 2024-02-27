91e2b612069865d717bbd1e6ca169612dbe28c41
b2014648 <datb2014648@student.ctu.edu.vn>
2024-02-27 09:08:55 -0800

    Dinh nghia lop tro giup ket noi va lop truy xuat CSDL

---
+++ app/services/contact.service.js
@@ -0,0 +1,10 @@
+const { ObjectId } = require("mongodb");
+
+class ContactService {
+    constructor(client) {
+        this.Contact = client.db().collection("contacts");
+    }
+    // Định nghĩa các phương thức truy xuất CSDL sử dụng mongodb API
+}
+
+module.exports = ContactService;
